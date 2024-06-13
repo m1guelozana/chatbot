ea92e65f6a842a39b06a8194647756f9da8be51d
Miguel Ozana <miguelozana74@gmail.com>
2024-06-13 11:41:03 -0300

    new commit

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,39 @@ import qrcode from "qrcode-terminal";
 let client: Client;
 
 // const imports
-const idClient = 'BOT-MGL';
+const idClient = "BOT-MGL";
+
+// Functions
+async function waitForHumanAssistanceEnd(client: Client, chatId: string) {
+  return new Promise<void>((resolve) => {
+    const interval = setInterval(async () => {
+      const messages = await client
+        .getChatById(chatId)
+        .then((chat) => chat.fetchMessages({ limit: 10 }));
+      const humanEnded = messages.some(
+        (msg) => msg.body.toLowerCase() === "atendimento encerrado"
+      );
+
+      if (humanEnded) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 60000);
+  });
+}
 
 client = new Client({
   authStrategy: new LocalAuth({ clientId: idClient }),
   puppeteer: {
     headless: true,
     args: [
-      "--no-sandbox", 
+      "--no-sandbox",
       "--disable-steuid-sandbox",
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--disable-gpu'
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--disable-gpu",
     ],
     timeout: 0,
   },
@@ -30,30 +49,30 @@ client = new Client({
   },
 });
 
-client.on('ready', () => {
-  console.log('WhatsApp Client initialized');
+client.on("ready", () => {
+  console.log("WhatsApp Client initialized");
 });
 
-client.on('qr', (qr: string) =>{
-  console.log('QR RECIVED')
+client.on("qr", (qr: string) => {
+  console.log("QR RECIVED");
   qrcode.generate(qr, { small: true });
 });
 
-client.on('authenticated', () => {
-  console.log('© BOT-MGL Autenticado');
+client.on("authenticated", () => {
+  console.log("© BOT-MGL Autenticado");
 });
 
-client.on('auth_failure', function() {
-  console.error('© BOT-MGL Falha na autenticação');
+client.on("auth_failure", function () {
+  console.error("© BOT-MGL Falha na autenticação");
 });
 
-client.on('change_state', state => {
-console.log('© BOT-MGL Status de conexão: ', state );
+client.on("change_state", (state) => {
+  console.log("© BOT-MGL Status de conexão: ", state);
 });
 
-client.on('message_create', async msg => {
+client.on("message_create", async (msg) => {
   // Lógica para tratar grupos
-  if (msg.body !== null && !msg.from.includes('@c.us')) return null;
+  if (msg.body !== null && !msg.from.includes("@c.us")) return null;
 
   // Lógica para tratar ciphertext
   if (msg.type.toLowerCase() == "e2e_notification") return null;
@@ -63,11 +82,30 @@ client.on('message_create', async msg => {
 
 await client.initialize();
 
-
-client.on('message', async msg =>{
-  if (msg.body !== null && msg.from.includes('@c.us')){
-    client.sendMessage(msg.from, 'Olá! Tudo bem? Obrigado por entrar em contato. Este é um atendimento automatizado. Precisa de alguma ajuda ou gostaria de conversar com um humano?')
+client.on("message", async (msg) => {
+  if (msg.body !== null && msg.from.includes("@c.us")) {
+    client.sendMessage(
+      msg.from,
+      "Olá! Tudo bem? Obrigado por entrar em contato. Este é um atendimento automatizado. Precisa de alguma ajuda ou gostaria de conversar com um humano?"
+    );
   }
-  
-});
 
+  if (msg.from.includes("Humano")) {
+    client.sendMessage(
+      msg.from,
+      "Você foi transferido para um atendimento humano. Por favor, aguarde."
+    );
+    await waitForHumanAssistanceEnd(client, msg.from);
+  } else {
+    try {
+      if (msg) {
+        console.log(`Sending Message ${msg.from}`);
+        await client.sendMessage(
+          msg.from,
+          "Olá! Tudo bem? Escolha uma opção a baixo para continuarmos seu atendimento."
+        );
+        await client.sendMessage(msg.from, "[1] Realizar Agendamento\n[3] Finalizar Atendimento");
+      }
+    } catch (err) {}
+  }
+});
